efc0ebca0052cc2d4bec5359c79c0aae73e81260
Eliezer Ribeiro <146236086+eliezershell@users.noreply.github.com>
2023-10-27 20:47:23 -0300

    Create instalador_wordpress6.3.2.sh

---
+++ instalador_wordpress6.3.2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install apache2 libapache2-mod-php -y
+sudo a2dismod mpm_event
+sudo a2enmod mpm_prefork rewrite php8.1
+sudo systemctl restart apache2
+
+sudo apt install php8.1 php-json php-mysqli php-curl php-dom php-exif php-fileinfo php-igbinary php-imagick php-intl php-mbstring php-xml php-zip php-apcu php-opcache php-iconv php-shmop php-simplexml php-xmlreader php-ssh2 php-ftp php-sockets -y
+
+sudo apt install mariadb-server -y
+read -p "Nome de usuário MySQL: " usuario
+read -p "Senha de usuário MySQL: " senha
+read -p "Nome do banco de dados MySQL: " banco
+sudo mysql -u root <<MYSQL_SCRIPT
+CREATE USER '$usuario'@'localhost' IDENTIFIED BY '$senha';
+CREATE DATABASE $banco;
+GRANT ALL PRIVILEGES ON $banco.* TO '$usuario'@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
+sudo apt install unzip -y
+sudo wget -P /var/www/html https://wordpress.org/latest.zip
+sudo rm -f /var/www/html/index.html
+sudo unzip /var/www/html/latest.zip -d /var/www/html/
+sudo rm -f /var/www/html/latest.zip
+sudo mv /var/www/html/wordpress/* /var/www/html/
+sudo rmdir /var/www/html/wordpress/
+sudo chown -R www-data /var/www/
+sudo chmod 755 -R /var/www/
+
+sudo systemctl restart apache2
+sudo systemctl status apache2
+
+echo "------------------------------Instalação concluída com sucesso!------------------------------"
+echo "----------------------Script by: Eliezer Ribeiro | linkedin.com/in/elinux--------------------"
+exit 0
